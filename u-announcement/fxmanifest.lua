1012cefb3af39bd94d6a42d083e056cc0f1db92b
Uggish Resources <104546122+Uggish@users.noreply.github.com>
2023-09-07 23:23:40 +0200

    Update fxmanifest.lua

--- u-announcement/fxmanifest.lua
+++ u-announcement/fxmanifest.lua
@@ -1,6 +1,13 @@
-lua54 "yes"
+lua54 'yes'
 fx_version 'cerulean'
-game "gta5"
-author " Uggish Resources"
-description "Simple - AnnounceMent Notis"
-client_script "client.lua"
+game 'gta5'
+author 'Uggish Resources'
+description 'Simple - AnnounceMent Notis'
+shared_script {
+    '@ox_lib/init.lua'
+} 
+
+client_script {
+    'config.lua',
+    'client.lua'
+}
