34bc9e1de83974d7b5fa6ce60ede6b60691236ec
Uggish Resources <104546122+Uggish@users.noreply.github.com>
2023-09-07 23:19:42 +0200

    Update client.lua, added support for ox-lib, ox-contextmenu!

--- u-announcement/client.lua
+++ u-announcement/client.lua
@@ -1,55 +1,93 @@
 QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterCommand('announce', function()
-exports['qb-menu']:openMenu({
-    {
-        header = 'Notification to all!',
-        txt = 'Click for more information.',
-        icon = 'fas fa-envelope',
-        params = {
-            event = 'menu:client:AnnounceMentMenu',
-            args = {
-            }
-        }
-    }
+
+    -- QB
+    if Config.UseQBCoreMenu then
+        RegisterCommand('announce', function()
+            exports['qb-menu']:openMenu({
+                {
+                    header = 'Notification to all!',
+                    txt = 'Click for more information.',
+                    icon = 'fas fa-envelope',
+                    params = {
+                        event = 'menu:client:AnnounceMentMenu',
+                        args = {}
+                    }
+                }
+            })
+        end)
     
-})
-end, "admin")
-
-RegisterNetEvent('menu:client:AnnounceMent')
-AddEventHandler('menu:client:AnnounceMent', function()
-    TriggerEvent('chatMessage', '^2Admin-Team> ^0Server restarting soon!')
-end)
-
-RegisterNetEvent('menu:client:AnnounceMentMenu')
-AddEventHandler('menu:client:AnnounceMentMenu', function()
-    exports['qb-menu']:openMenu({
-        {
-            header = 'Notification to all!',
-            txt = 'If you want to send out a message, press here.',
-            icon = 'fas fa-envelope',
-            params = {
-                event = 'menu:client:AnnounceMent',
-                args = {
+        RegisterNetEvent('menu:client:AnnounceMent')
+        AddEventHandler('menu:client:AnnounceMent', function()
+            TriggerEvent('chatMessage', '^2Admin-Team> ^0Server restarting soon!')
+        end)
+    
+        RegisterNetEvent('menu:client:AnnounceMentMenu')
+        AddEventHandler('menu:client:AnnounceMentMenu', function()
+            exports['qb-menu']:openMenu({
+                {
+                    header = 'Notification to all!',
+                    txt = 'If you want to send out a message, press here.',
+                    icon = 'fas fa-envelope',
+                    params = {
+                        event = 'menu:client:AnnounceMent',
+                        args = {}
+                    }
+                },
+                {
+                    header = 'GO BACK!',
+                    icon = 'fas fa-arrow-left',
+                    params = {
+                        event = 'menu:client:AnnouncementCloseMenu',
+                        args = {}
+                    }
+                }
+            })
+        end)
+    
+        RegisterNetEvent('menu:client:AnnouncementCloseMenu', function()
+            ExecuteCommand('announce')
+        end)
+    end
+    
+    -- OX
+    if not Config.UseQBCoreMenu then
+        lib.registerContext({
+            id = 'some_menu',
+            title = 'Message',
+            options = {
+                {
+                    title = 'Notification to all!',
+                    description = 'Click for more information.',
+                    icon = 'user',
+                    event = 'ox-menu:client:AnnounceMentMenu',
+                    arrow = true,
+                    args = {}
                 }
             }
-        },
-        {
-            header = 'GO BACK!',
-            icon = 'fas fa-arrow-left',
-            params = {
-                event = 'menu:client:AnnouncementCloseMenu',
-                args = {
-                }
-            }
-        }
-    })
-end)
+        })
     
-RegisterNetEvent('menu:client:AnnouncementCloseMenu', function()
-ExecuteCommand('announce')
-end)
-
--- Uggish Resources!
--- Discord: https://discord.gg/BQr99Kh5TC (And if you want support? Sure, join discord and open ticket!)
--- Website: https://uggish-store.tebex.io/
+        RegisterCommand('announce', function()
+            lib.showContext('some_menu')
+        end)
+    
+        RegisterNetEvent('ox-menu:client:AnnounceMentMenu', function(args)
+            lib.registerContext({
+                id = 'event_menu',
+                title = 'Message',
+                menu = 'some_menu',
+                options = {
+                    {
+                        title = 'Notification to all!',
+                        description = 'If you want to send out a message, press here.',
+                        icon = 'envelope',
+                        event = 'menu:client:AnnounceMent',
+                        arrow = true, 
+                        args = {}
+                    }
+                }
+            })
+    
+            lib.showContext('event_menu')
+        end)
+    end
